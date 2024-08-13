675258a17e413dbf49127b66a2741576dce2e537
Anthonyschools2024 <Anthony.Kimble@atlasschool.com>
2024-08-12 23:10:15 -0500

    quick_sort

--- sort.h
+++ sort.h
@@ -1,30 +1,30 @@
 #ifndef SORT_H
 #define SORT_H
-#include <stdlib.h>
-#include <stdio.h>
+
+#include <stddef.h>
 
 /**
  * struct listint_s - Doubly linked list node
+ * @n: Integer value of the node
+ * @prev: Pointer to the previous node in the list
+ * @next: Pointer to the next node in the list
  *
- * @n: Integer stored in the node
- * @prev: Pointer to the previous element of the list
- * @next: Pointer to the next element of the list
+ * Description: A node in a doubly linked list used for sorting.
  */
-
 typedef struct listint_s
 {
-	const int n;
-	struct listint_s *prev;
-	struct listint_s *next;
+    int n;
+    struct listint_s *prev;
+    struct listint_s *next;
 } listint_t;
 
-void bubble_sort(int *array, size_t size);
-void print_array(const int *array, size_t size);
-void print_list(const listint_t *list);
+/* Function prototypes */
 void insertion_sort_list(listint_t **list);
+void swap_nodes(listint_t **list, listint_t *node1, listint_t *node2);
+void print_list(const listint_t *list);
 void selection_sort(int *array, size_t size);
+void print_array(const int *array, size_t size);
 void quick_sort(int *array, size_t size);
-void quicksort(int *array, int low, int high, size_t size);
-size_t partition(int *array, int low, int high, size_t size);
+void bubble_sort(int *array, size_t size);
 
 #endif
